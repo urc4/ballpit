4abcd5817e33e9eccdcafba6e9d38a976c25928c
Beato <121357043+urc4@users.noreply.github.com>
2023-02-18 21:47:45 -0300

    Improve code readability

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const WIDTH = canvas.width;
 const HEIGHT = canvas.height;
-const ctx = canvas.getContext("2d");
+const CTX = canvas.getContext("2d");
 
 const COLOR_PALETTE = [
   "#66FF00",
@@ -12,22 +12,20 @@ const COLOR_PALETTE = [
   "#FFF000",
   "#FFAA1D",
   "#FF007F",
-  "white",
+  "#F2F2F2",
 ];
 
-function getRandomColor() {
-  return COLOR_PALETTE[Math.floor(Math.random() * COLOR_PALETTE.length)];
-  //   its zero inclusive and one exclusive
-}
+const getRandomColor = () =>
+  COLOR_PALETTE[Math.floor(Math.random() * COLOR_PALETTE.length)];
 
-function getRandomPosition() {
+const getRandomPosition = () => {
   position = {};
   position.x = Math.floor(Math.random() * WIDTH);
   position.y = Math.floor(Math.random() * HEIGHT);
   return position;
-}
+};
 
-function getRandomDirection() {
+const getRandomDirection = () => {
   direction = {};
   const x = Math.random();
   const y = Math.random();
@@ -35,71 +33,70 @@ function getRandomDirection() {
   direction.x = x / norm;
   direction.y = y / norm;
   return direction;
-}
+};
 
-function dotProduct(vec1, vec2) {
-  return vec1.x * vec2.x + vec1.y * vec2.y; //could use math.floor to reduce calculations
-}
+const dotProduct = (vec1, vec2) => vec1.x * vec2.x + vec1.y * vec2.y;
 
 class Ball {
   constructor(radius) {
-    this.speed = 3;
     this.radius = radius;
     this.color = getRandomColor();
 
     this.position = getRandomPosition();
-    if (this.position.x + 5 * this.radius > WIDTH)
+    if (this.position.x + this.radius > WIDTH)
       this.position.x = WIDTH - 5 * this.radius;
-    if (this.position.x - 5 * this.radius < 0)
+    else if (this.position.x - this.radius < 0)
       this.position.x = 5 * this.radius;
-    if (this.position.y + 5 * this.radius > HEIGHT)
+    if (this.position.y + this.radius > HEIGHT)
       this.position.y = HEIGHT - 5 * this.radius;
-    if (this.position.y - 5 * this.radius < 0)
+    else if (this.position.y - this.radius < 0)
       this.position.y = 5 * this.radius;
-    // also add to check if there are any oteher balls
 
-    this.direction = getRandomDirection();
+    const direction = getRandomDirection();
+    this.speed = 3;
     this.velocity = {
-      x: this.direction.x * this.speed,
-      y: this.direction.y * this.speed,
+      x: direction.x * this.speed,
+      y: direction.y * this.speed,
     };
+    this.changedDirection = false;
   }
 
   draw() {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.closePath();
+    CTX.beginPath();
+    CTX.fillStyle = this.color;
+    CTX.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
+    CTX.fill();
+    CTX.closePath();
   }
 
-  update() {
-    this.draw();
+  updateWallCollision() {
     const color = this.color;
+    // get rid of + this.velocity. ...?
     const leftSide = this.position.x + this.velocity.x - this.radius;
     const rightSide = this.position.x + this.velocity.x + this.radius;
     const bottomSide = this.position.y + this.velocity.y + this.radius;
     const topSide = this.position.y + this.velocity.y - this.radius;
-    // wall collision
+
     if (leftSide < 0 || rightSide > WIDTH) {
-      // if (this.velocity.x < 0) this.velocity.x = --this.velocity.x * -1;
-      // else this.velocity.x = ++this.velocity.x * -1;
       this.velocity.x = -this.velocity.x;
-      do {
-        this.color = getRandomColor();
-      } while (this.color === color);
-      // gotta make sure it does not get the same value
-      // make a return statement with ||
+      this.changedDirection = true;
     }
     if (topSide < 0 || bottomSide > HEIGHT) {
-      // if (this.velocity.y < 0) this.velocity.y = --this.velocity.y * -1;
-      // else this.velocity.y = ++this.velocity.y * -1;
       this.velocity.y = -this.velocity.y;
+      this.changedDirection = true;
+    }
+
+    if (this.changedDirection) {
       do {
         this.color = getRandomColor();
       } while (this.color === color);
     }
+    this.changedDirection = false;
+  }
 
+  update() {
+    this.draw();
+    this.updateWallCollision();
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
   }
@@ -107,84 +104,149 @@ class Ball {
 
 class BallPit {
   constructor(pitInit) {
-    this.ballRadius = pitInit.ballRadius;
     this.balls = [];
     for (let i = 0; i < pitInit.numberBalls; i++) {
-      this.balls.push(new Ball(this.ballRadius));
+      this.balls.push(new Ball(pitInit.ballRadius));
     }
     this.collisionMatrix = [];
-    for (let i = 0; i < pitInit.numberBalls; i++) {
-      this.collisionMatrix[i] = new Array(pitInit.numberBalls).fill(false);
+    for (let i = 0; i < this.balls.length; i++) {
+      this.collisionMatrix[i] = new Array(this.balls.length).fill(false);
     }
     // gotta make sure they are spread out when initiated
+    this.updateStartPositions();
   }
-  addBall() {
-    this.balls.push(new Ball(this.ballRadius));
+  addBall(radius) {
+    //NOT WORKING PROPERLY
+    this.balls.push(new Ball(radius || 50));
+    // would also have to create a new row and column in colisionmatrix
+    // gotta also start in a valid place
+    // can update collisino matrix and check it last row is all false if not get a new position
+    this.collisionMatrix = [];
+    for (let i = 0; i < this.balls.length; i++) {
+      this.collisionMatrix[i] = new Array(this.balls.length).fill(false);
+    }
+    this.updateCollisionMatrix();
+    const lastBallIndex = this.balls.length - 1;
+    let isValidPosition = false;
+    while (!isValidPosition) {
+      for (let j = 0; j < this.collisionMatrix[lastBallIndex].length; j++) {
+        isValidPosition =
+          isValidPosition || !this.collisionMatrix[lastBallIndex][j];
+      }
+      this.balls.position = getRandomPosition();
+    }
   }
   removeBall() {
+    //NOT WORKING PROPERLY
     this.balls.pop();
+    // updateMatrixColision
   }
 
-  checkIntersection(ballOne, ballTwo) {
+  checkIntersection(ball_1, ball_2) {
     const distanceSquared =
-      (ballTwo.position.x +
-        ballTwo.velocity.x -
-        (ballOne.position.x + ballOne.velocity.x)) **
+      (ball_2.position.x +
+        ball_2.velocity.x -
+        (ball_1.position.x + ball_1.velocity.x)) **
         2 +
-      (ballTwo.position.y +
-        ballTwo.velocity.y -
-        (ballOne.position.y + ballOne.velocity.y)) **
+      (ball_2.position.y +
+        ball_2.velocity.y -
+        (ball_1.position.y + ball_1.velocity.y)) **
         2;
-    const minimumDistanceSquared = (ballOne.radius + ballTwo.radius) ** 2;
+    const minimumDistanceSquared = (ball_1.radius + ball_2.radius) ** 2;
     return distanceSquared < minimumDistanceSquared;
   }
 
   updateCollisionMatrix() {
     for (let i = 0; i < this.balls.length; i++) {
       for (let j = i + 1; j < this.balls.length; j++) {
-        const ballOne = this.balls[i];
-        const ballTwo = this.balls[j];
-        const intersect = this.checkIntersection(ballOne, ballTwo);
+        const ball_1 = this.balls[i];
+        const ball_2 = this.balls[j];
+        const intersect = this.checkIntersection(ball_1, ball_2);
         this.collisionMatrix[i][j] = intersect;
         this.collisionMatrix[j][j] = intersect;
       }
     }
   }
-  // basics elastic collisions with vectors
-  changeDirections(ballOne, ballTwo) {
-    ballOne.color = "red";
-    ballTwo.color = "blue";
-    const distance = Math.sqrt(
-      (ballTwo.position.x - ballOne.position.x) ** 2 +
-        (ballTwo.position.y - ballOne.position.y) ** 2
+  // basics elastic collisions with vectors gotta add weight here
+  changeDirections(ball_1, ball_2) {
+    const colorOne = ball_1.color;
+    const colorTwo = ball_2.color;
+    do {
+      ball_1.color = getRandomColor();
+    } while (ball_1.color === colorOne);
+    do {
+      ball_2.color = getRandomColor();
+    } while (ball_2.color === colorTwo);
+    const distance = Number(
+      Math.sqrt(
+        (ball_2.position.x +
+          ball_2.velocity.x -
+          ball_1.position.x -
+          ball_1.velocity.x) **
+          2 +
+          (ball_2.position.y +
+            ball_2.velocity.y -
+            ball_1.position.y -
+            ball_1.velocity.y) **
+            2
+      ).toFixed(3)
     );
     const distanceVector = {
-      x: (ballTwo.position.x - ballOne.position.x) / distance,
-      y: (ballTwo.position.y - ballOne.position.y) / distance,
+      x:
+        (ball_2.position.x +
+          ball_2.velocity.x -
+          ball_1.position.x -
+          ball_1.velocity.x) /
+        distance,
+      y:
+        (ball_2.position.y +
+          ball_2.velocity.y -
+          ball_1.position.y -
+          ball_2.velocity.y) /
+        distance,
     };
 
-    const ballOneNewDirection = {
+    const ball_1NewDirection = {
       x:
-        ballOne.velocity.x -
-        2 * dotProduct(ballOne.velocity, distanceVector) * distanceVector.x,
+        ball_1.velocity.x -
+        2 * dotProduct(ball_1.velocity, distanceVector) * distanceVector.x,
       y:
-        ballOne.velocity.y -
-        2 * dotProduct(ballOne.velocity, distanceVector) * distanceVector.y,
+        ball_1.velocity.y -
+        2 * dotProduct(ball_1.velocity, distanceVector) * distanceVector.y,
     };
-    const ballTwoNewDirection = {
+    const ball_2NewDirection = {
       x:
-        ballTwo.velocity.x -
-        2 * dotProduct(ballTwo.velocity, distanceVector) * distanceVector.x,
+        ball_2.velocity.x -
+        2 * dotProduct(ball_2.velocity, distanceVector) * distanceVector.x,
       y:
-        ballTwo.velocity.y -
-        2 * dotProduct(ballTwo.velocity, distanceVector) * distanceVector.y,
+        ball_2.velocity.y -
+        2 * dotProduct(ball_2.velocity, distanceVector) * distanceVector.y,
     };
-    console.log(ballOneNewDirection.x ** 2 + ballOneNewDirection.y ** 2);
-    console.log(ballTwoNewDirection);
-    ballOne.velocity.x = ballOneNewDirection.x;
-    ballOne.velocity.y = ballOneNewDirection.y;
-    ballTwo.velocity.x = ballTwoNewDirection.x;
-    ballTwo.velocity.y = ballTwoNewDirection.y;
+    const normOne = Math.sqrt(
+      ball_1NewDirection.x ** 2 + ball_1NewDirection.y ** 2
+    );
+    const normTwo = Math.sqrt(
+      ball_2NewDirection.x ** 2 + ball_2NewDirection.y ** 2
+    );
+    ball_1.velocity.x = (ball_1NewDirection.x * ball_1.speed) / normOne;
+    ball_1.velocity.y = (ball_1NewDirection.y * ball_1.speed) / normOne;
+    ball_2.velocity.x = (ball_2NewDirection.x * ball_2.speed) / normTwo;
+    ball_2.velocity.y = (ball_2NewDirection.y * ball_2.speed) / normTwo;
+    // when they are going the same positive direction it just breaks
+    // thats because the velocity is a littlw f=differnet from rounding I guess
+  }
+
+  updateStartPositions() {
+    this.updateCollisionMatrix();
+    for (let i = 0; i < this.balls.length; i++) {
+      for (let j = i + 1; j < this.balls.length; j++) {
+        while (this.collisionMatrix[i][j]) {
+          this.balls[j].position = getRandomPosition(
+            this.updateCollisionMatrix()
+          );
+        }
+      }
+    }
   }
 
   updateCollisions() {
@@ -209,16 +271,15 @@ class BallPit {
 // create balls
 const pitInit = {
   ballRadius: 20,
-  numberBalls: 5,
+  numberBalls: 10,
 };
-const ballPit = new BallPit(pitInit);
-// recursive callback function to update screen display
+
+const ballPit = new BallPit(pitInit); //does new keyword return a pointer?
+
 function animate() {
-  //   ctx.fillStyle = "black";
-  //   ctx.fillRect(0, 0, WIDTH, HEIGHT);
-  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+  CTX.clearRect(0, 0, WIDTH, HEIGHT);
   ballPit.update();
   requestAnimationFrame(animate);
 }
-
+// recursive callback function to update screen display
 animate();
